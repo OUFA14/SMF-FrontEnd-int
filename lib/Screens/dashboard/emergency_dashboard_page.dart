0aec5596ceceee15999eec014bbfcf71805b5ce6
OUFA14 <seifh0307@gmail.com>
2026-05-29 15:28:57 +0300

    Update emergency_dashboard_page.dart

--- lib/Screens/dashboard/emergency_dashboard_page.dart
+++ lib/Screens/dashboard/emergency_dashboard_page.dart
@@ -8,8 +8,8 @@ import '../../models/emergency_contact.dart';
 import '../../models/emergency_system.dart';
 import 'package:flutter/material.dart';
 import 'package:lucide_flutter/lucide_flutter.dart';
-
 import '../../theme/app_theme.dart';
+
 class EmergencyDashboardPage extends StatefulWidget {
   const EmergencyDashboardPage({super.key});
 class _EmergencyDashboardPageState extends State<EmergencyDashboardPage>
